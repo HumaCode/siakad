0d998adc7e47a04698c33bde0e81a1a7460d065b
HumaCode <humaidizakaria1998@gmail.com>
2026-06-09 10:52:05 +0700

    style: fix modal positioning by adding justify-center to base Modal dialog container

--- resources/js/Components/Modal.tsx
+++ resources/js/Components/Modal.tsx
@@ -42,7 +42,7 @@ export default function Modal({
             <Dialog
                 as="div"
                 id="modal"
-                className="fixed inset-0 z-[1050] flex transform items-center overflow-y-auto px-4 py-6 transition-all sm:px-0"
+                className="fixed inset-0 z-[1050] flex transform items-center justify-center overflow-y-auto px-4 py-6 transition-all sm:px-0"
                 onClose={close}
             >
                 <TransitionChild
